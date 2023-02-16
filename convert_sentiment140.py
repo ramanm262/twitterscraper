d8e361ead9591b325bed1020f5ba0c910a0139ca
ramanm262 <rm1277@unh.edu>
2023-02-16 15:36:00 -0500

    Expanded training set
    There is a weird byte read issue in tweet 232719 of the training set, so `convert_sentiment140.py` has been changed to read up to 232719 instead of 160000. This doesn't decrease processing time much, and seems to have a good effect on the clustering quality.

--- convert_sentiment140.py
+++ convert_sentiment140.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 tweets = pd.read_csv("resources/sentiment140_data/training.1600000.processed.noemoticon.csv",
-                     names=["polarity", "id", "date", "query", "user", "text"], nrows=160000)
+                     names=["polarity", "id", "date", "query", "user", "text"], nrows=232719)
 tweets.rename(columns={"text": "Text"}, inplace=True)
 tweets.to_csv("resources/stream_output.txt")
